c4d87e455ec05aaefb5c1719002a45e586fc3fa0
Taher Lakdawala <Taher.Lakdawala@harbinger.in>
2018-06-27 12:57:59 +0530

    Adding port to JS

--- node-server/server.js
+++ node-server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.set('View Engine','hbs');
@@ -42,6 +43,6 @@ app.get('/bad',(req,res) => {
   });
 });
 
-app.listen(3000,()=>{
-  console.log('Server running on port 3000');
+app.listen(port,()=>{
+  console.log('Server running on port: ' + port);
 });
